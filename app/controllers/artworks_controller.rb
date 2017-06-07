acb640c170f97b66790b98c89b74abf89b4c6507
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-06-07 16:06:51 +0300

    add index and create actions to ArtworksController

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,22 @@
 class ArtworksController < ApplicationController
+    def index
+        @artworks = Artwork.all
+        render json: @artworks, status: :ok 
+    end
+
+    def create
+        @artist = Artist.find(artwork_params[:artist_id])
+        @artworks = @artist.artworks.create(artwork_params)
+        render json: @artworks, status: :created
+    end
+
+    private
+    
+    def artwork_params
+        params.permit(:artist_id, :title, :description, :price, :width, :height, :status, :images)
+    end
+
+    def set_artist
+        @artworks = Artwork.find(params[:id])
+    end
 end
