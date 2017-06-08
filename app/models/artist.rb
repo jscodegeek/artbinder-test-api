97941bf76487e2da99a322c760fe3d93324bcb75
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-06-08 11:29:24 +0300

    add validations for Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,13 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class Artist < ApplicationRecord
+    validates :first_name, presence: true
+    validates :email, presence: true, uniqueness: true, email: true
     has_many :artworks, dependent: :destroy
 end
