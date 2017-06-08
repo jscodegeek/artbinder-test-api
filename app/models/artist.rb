a9475694e769d9b13365f041d353fb2cf405770f
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-06-08 17:22:26 +0300

    minor fixes

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
+      record.errors[attribute] << (options[:message] || "Email is not valid")
     end
   end
 end
@@ -9,5 +9,6 @@ end
 class Artist < ApplicationRecord
     validates :first_name, presence: true
     validates :email, presence: true, uniqueness: true, email: true
+    
     has_many :artworks, dependent: :destroy
 end
