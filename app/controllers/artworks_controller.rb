9d48ec709e10f2a45b31cd09af2e0e138fde0897
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-06-08 13:52:17 +0300

    handling images property absence in the artwork payload

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -27,7 +27,7 @@ class ArtworksController < ApplicationController
     def create
         @artist = Artist.find(artwork_params[:artist_id])
         @artwork = @artist.artworks.create(artwork_params)
-        images = params[:images]
+        images = params[:images] || []
         images.each do |image|
             @artwork.image_files.create({:name => image[:name], :base64 => image[:base64]})
         end
