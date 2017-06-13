98406867b2070961921f67533d61f4031f8da806
Eugene Safronov <evgeniy.safronov@outlook.com>
2017-06-14 01:38:47 +0300

    artworks_controller refactoring

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -3,60 +3,26 @@ class ArtworksController < ApplicationController
 
     def index
         @artworks = Artwork.all
-        app_url = request.base_url
-        
-        response = @artworks.map do |aw|
-            aw_id = aw.id
-            aw = aw.as_json
-            aw[:images] = []
-                
-            dir_name = "public/#{APP_CONFIG[:image_folder]}/#{aw_id}"
-            if File.exists?(dir_name)
-                files = Dir.entries(dir_name).select {|f| !File.directory? f}
-                files.each do |name|
-                    aw[:images] << "#{app_url}/#{APP_CONFIG[:image_folder]}/#{aw_id}/#{name}"
-                end
-            end            
-            aw
-        end
-        render json: response, status: :ok 
+        render json: @artworks, status: :ok 
     end
 
     def create
         @artist = Artist.find(artwork_params[:artist_id])
         @artwork = @artist.artworks.create(artwork_params)
         images = params[:images] || []
-        response = @artwork.as_json
-        response[:message] = []
         images.each do |image|
             begin  
                 @artwork.image_files.create({:name => image[:name], :base64 => image[:base64]})  
-            rescue  
-                response[:message] << "something wrong with #{image[:name]}, file isn't saved"  
+            rescue
+                render status: :bad_request  
             end
         end
 
-        render json: response, status: :created
+        render json: @artwork, status: :created
     end
 
     def show
-        app_url = request.base_url
-        
-        aw_id = @artwork.id
-
-        puts ImageFile.find_file_paths_by_aw_id(aw_id)
-
-        response = @artwork.as_json
-        response[:images] = []
-        dir_name = "public/#{APP_CONFIG[:image_folder]}/#{aw_id}"
-        if File.exists?(dir_name)
-            files = Dir.entries(dir_name).select {|f| !File.directory? f}
-            files.each do |name|
-                response[:images] << "#{app_url}/#{APP_CONFIG[:image_folder]}/#{aw_id}/#{name}"
-            end
-        end 
-
-        render json: response, status: :ok
+        render json: { :artwork => @artwork, :images => ImageFile.find_file_paths_by_aw_id(@artwork.id), :base_url => request.base_url}, status: :ok
     end
 
     def update
